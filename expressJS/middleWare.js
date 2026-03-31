813599ed1012360432240d1e772599cc94755108
Aman kumar singh <157352533+aman-singh0409@users.noreply.github.com>
2026-03-31 22:05:33 +0530

    learned about form handling

--- expressJS/middleWare.js
+++ expressJS/middleWare.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(function(req,res,next){
     console.log('Middleware run');
     next();
@@ -15,6 +19,8 @@ app.get("/about",function(req,res){
   res.send("This is an about page");
 });
 
+
+//error handler part
 app.get("/profile",function(req,res,next){
   return next(new Error("Something wnet wrong"))
 });
